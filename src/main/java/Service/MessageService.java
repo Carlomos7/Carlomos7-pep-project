f02aeea58ad3176515f4209b3b81f7037769ac27
Carlos Segarra <cs525@njit.edu>
2025-06-27 21:58:40 +0000

    Feat: cascaded rest of relevant DAO methods through service class

--- src/main/java/Service/MessageService.java
+++ src/main/java/Service/MessageService.java
@@ -22,7 +22,39 @@ public class MessageService {
         }
         return messageDAO.insertMessage(message);
     }
+    public List<Message> getAllMessages(){
+        return messageDAO.getAllMessages();
+    }
 
+    public Message getMessageById(int id){
+        return messageDAO.getMessageById(id);
+    }
+
+    public boolean deleteMessageById(int id){
+        Message found = messageDAO.getMessageById(id);
+        if(found == null){
+            return false;
+        }
+        messageDAO.deleteMessageById(id);
+        return true;
+    }
+
+    public Message updateMessage(int id, String text){
+        if(!isValidMessageText(text)){
+            return null;
+        }
+        Message existing = messageDAO.getMessageById(id);
+        if (existing == null){
+            return null;
+        }
+        messageDAO.updateMessageText(id, text);
+        return messageDAO.getMessageById(id);
+    }
+
+    public List<Message> getMessagesByAccountId(int id){
+        return messageDAO.getMessagesByAccountId(id);
+    }
+    
     private boolean isValidMessageText(String text){
         return text != null && !text.isBlank() && text.length() <= 255;
     }
