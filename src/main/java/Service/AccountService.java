ac22f2b471034cfa26cc36c3422a696688199085
Carlos Segarra <cs525@njit.edu>
2025-06-27 21:56:20 +0000

    Feat: added register and login methods

--- src/main/java/Service/AccountService.java
+++ src/main/java/Service/AccountService.java
@@ -14,4 +14,24 @@ public class AccountService {
         this.accountDAO = accountDAO;
     }
 
+    public Account register(Account account){
+        String username = account.getUsername();
+        String password = account.getPassword();
+
+        if(username == null || username.isBlank()){
+            return null;
+        }
+        if (password == null || password.length() < 4){
+            return null;
+        }
+        boolean duplicateUsername = accountDAO.getAccountByUsername(username) != null;
+        if(duplicateUsername){
+            return null;
+        }
+        return accountDAO.insertAccount(account);
+    }
+
+    public Account login(String username, String password){
+        return accountDAO.getAccountByUsernameAndPassword(username, password);
+    }
 }
