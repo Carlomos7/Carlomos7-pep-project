fe7776030ef984fffd8871d7ec7bb3c47d3dd924
Carlos Segarra <cs525@njit.edu>
2025-06-27 21:50:23 +0000

    Feat: insertAccount sql method

--- src/main/java/DAO/AccountDAO.java
+++ src/main/java/DAO/AccountDAO.java
@@ -7,5 +7,24 @@ import java.sql.*;
 
 
 public class AccountDAO {
-
+    public Account insertAccount(Account account){
+        Connection conn = ConnectionUtil.getConnection();
+        try{
+            String sql = "INSERT INTO account (username, password) VALUES (?,?)";
+            PreparedStatement ps = conn.prepareStatement(sql, Statement.RETURN_GENERATED_KEYS);
+            
+            ps.setString(1, account.getUsername());
+            ps.setString(2, account.getPassword());
+            ps.executeUpdate();
+            
+            ResultSet rs = ps.getGeneratedKeys();
+            if(rs.next()){
+                int id = rs.getInt(1);
+                return new Account(id, account.getUsername(), account.getPassword());
+            }
+        } catch (SQLException e){
+            System.out.println(e.getMessage());
+        }
+        return null;
+    }
 }
