f88dd121f46715f6c999e165c1b6588008c7e44c
Carlos Segarra <cs525@njit.edu>
2025-06-27 21:53:21 +0000

    Feat: created insert and getAllMessages methods

--- src/main/java/DAO/MessageDAO.java
+++ src/main/java/DAO/MessageDAO.java
@@ -9,5 +9,52 @@ import java.util.List;
 
 public class MessageDAO {
     
+    public Message insertMessage(Message message){
+        Connection conn = ConnectionUtil.getConnection();
+        try{
+            String sql = "INSERT INTO message (posted_by, message_text, time_posted_epoch) VALUES(?,?,?)";
+            PreparedStatement ps = conn.prepareStatement(sql, Statement.RETURN_GENERATED_KEYS);
+
+            ps.setInt(1,message.getPosted_by());
+            ps.setString(2,message.getMessage_text());
+            ps.setLong(3,message.getTime_posted_epoch());
+
+            ps.executeUpdate();
+            ResultSet rs = ps.getGeneratedKeys();
+            if(rs.next()){
+                int messageId = rs.getInt(1);
+                return new Message(messageId,
+                    message.getPosted_by(), 
+                    message.getMessage_text(),
+                    message.getTime_posted_epoch());
+            }
+        } catch (SQLException e){
+            System.out.println(e.getMessage());
+        }
+        return null;
+    }
+
+    public List<Message> getAllMessages() {
+        Connection conn = ConnectionUtil.getConnection();
+        List<Message> messages = new ArrayList<>();
+        try{
+            String sql = "SELECT * FROM message";
+            PreparedStatement ps = conn.prepareStatement(sql);
+            
+            ResultSet rs = ps.executeQuery();
+            while(rs.next()){
+                Message message = new Message(
+                    rs.getInt("message_id"),
+                    rs.getInt("posted_by"),
+                    rs.getString("message_text"),
+                    rs.getLong("time_posted_epoch"));
+                messages.add(message);
+            }
+        } catch (SQLException e){
+            System.out.println(e.getMessage());
+        }
+        return messages;
+    }
+
 
 }
